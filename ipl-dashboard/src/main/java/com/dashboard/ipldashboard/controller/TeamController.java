551d7d61ba1791331cd0f0d102bf893aae842cb6
Rijwan <rijwank@ohumhealthcare.com>
2022-03-17 15:31:39 +0530

    React App with Team page

--- ipl-dashboard/src/main/java/com/dashboard/ipldashboard/controller/TeamController.java
+++ ipl-dashboard/src/main/java/com/dashboard/ipldashboard/controller/TeamController.java
@@ -6,11 +6,13 @@ import com.dashboard.ipldashboard.repository.TeamRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class TeamController {
 
   private TeamRepository teamRepository;
